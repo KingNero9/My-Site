d65fc81fceda7e2bab8f5545dfa692d2f4ee57dc
Tali Shushan <talishush@gmail.com>
2021-08-15 03:52:01 -0700

    added a thank you page

--- php/form-handler.php
+++ php/form-handler.php
@@ -1,25 +1,23 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
 
-$email_from = 'https://tali-shushan.website';
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
 
-$email_subject = "New Form Submission";
+    $mailTo = "talishush@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an e-mail from: ".$name.".\n\n".$message;
 
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"Subject: $subject.\n".
-"User Message: $message.\n";
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
 
-$to = 'talishush@gmail.com';
 
-$headers = "From: $email_from\r\n";
-
-$headers .="Reply-To: $visitor_email \r\n";
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: contact.html");
 ?>
+
+
+
+
+
