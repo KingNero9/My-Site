55e7343148078a7475ec21445bed7f5a232f78c8
Tali Shushan <talishush@gmail.com>
2021-08-18 03:04:23 -0700

    contact me is working!

--- php/form-handler.php
+++ php/form-handler.php
@@ -2,21 +2,22 @@
 
 if(isset($_POST['submit'])) {
     $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
     $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
-
+    
     $mailTo = "talishush@gmail.com";
     $headers = "From: ".$mailFrom;
-    $txt = "You have recieved an e-mail from: ".$name.".\n\n".$message;
-
+    $txt = "Message from your website: .$name.".\n\n".$message;
+    
     mail($mailTo, $subject, $txt, $headers);
     header("Location:https://tali-shushan.website/thankyou-page.html");
     exit;
 }
 
-
 ?>
+
+
 
 
 
